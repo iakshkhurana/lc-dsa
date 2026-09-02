cc1290aa4d5ae3555cfebc0fe87377e40769cda3
Aksh Khurana <150786734+iakshkhurana@users.noreply.github.com>
2026-09-02 13:36:59 +0530

    Time: 1258 ms (68.37%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,7 @@
-SELECT b.unique_id, a.name
-FROM Employees a
-LEFT JOIN EmployeeUNI b ON a.id = b.id;
+-- SELECT b.unique_id, a.name
+-- FROM Employees a
+-- LEFT JOIN EmployeeUNI b ON a.id = b.id;
+
+select b.unique_id, a.name
+from employeeuni b
+right join employees a on b.id=a.id;
