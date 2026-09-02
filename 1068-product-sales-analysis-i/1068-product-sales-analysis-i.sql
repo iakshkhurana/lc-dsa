0adb5ff381f6d07b8002feb5c73b828f6962751b
Aksh Khurana <150786734+iakshkhurana@users.noreply.github.com>
2026-09-02 13:43:56 +0530

    Time: 1432 ms (38.05%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,3 @@
-select p.product_name, s.year, s.price
-from product p
-cross join sales s
-where s.product_id=p.product_id;
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+INNER JOIN Product p ON s.product_id = p.product_id;
